16bebb9ec72beaf637a1a98a466ba8e0e00aea03
AllenLVieira <allenvieira96@gmail.com>
2024-11-17 19:30:33 -0300

    feat: add product entity

---
+++ src/main/java/br/com/allen/dynamodbcrud/model/entity/Product.java
@@ -0,0 +1,44 @@
+package br.com.allen.dynamodbcrud.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamoDbBean
+public class Product {
+  private String id;
+  private String name;
+  private BigDecimal price;
+
+  @DynamoDbPartitionKey
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+}
