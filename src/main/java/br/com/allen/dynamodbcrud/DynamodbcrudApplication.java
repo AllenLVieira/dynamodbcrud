c93684dffaa2a2540f2a161e75fb71a9c60710ea
AllenLVieira <allenvieira96@gmail.com>
2024-11-17 19:13:43 -0300

    chore: initial commit

---
+++ src/main/java/br/com/allen/dynamodbcrud/DynamodbcrudApplication.java
@@ -0,0 +1,12 @@
+package br.com.allen.dynamodbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbcrudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DynamodbcrudApplication.class, args);
+  }
+}
